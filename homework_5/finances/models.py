991d779cba3dff0caea96593a5216f528244a9a1
Aleksandr <allxf95@gmail.com>
2016-11-16 04:18:23 +0300

    Change models in project

--- homework_5/finances/models.py
+++ homework_5/finances/models.py
@@ -4,9 +4,21 @@ from django.core.urlresolvers import reverse
 from django.db import models
 
 
+class Account(models.Model):
+    number = models.PositiveIntegerField(primary_key=True)
+
+    def __str__(self):
+        return str(self.number)
+
+    @staticmethod
+    def get_absolute_url():
+        return reverse("charges:main")
+
+
 class Charge(models.Model):
     value = models.DecimalField(max_digits=8, decimal_places=2)
     date = models.DateField(default=datetime.today)
+    account = models.ForeignKey(Account)
 
     def __str__(self):
         return str(self.date) + " " + str(self.value)
