c64dd42a61b5ced7511d669676b81540284886fa
IIpocTo <allxf95@gmail.com>
2016-11-24 18:24:50 +0300

    Pull homework_6 as homework_7

---
+++ homework_7/finances/forms.py
@@ -0,0 +1,51 @@
+from datetime import date
+from decimal import Decimal
+
+from django import forms
+
+from .models import Charge, Account
+
+
+class AccountForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = [
+            "number"
+        ]
+
+
+class AccountLookForForm(forms.Form):
+    number = forms.CharField(
+        max_length=12,
+        min_length=12
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        number = cleaned_data.get('number')
+        account = Account.objects.filter(number=number)
+        if account is None:
+            self.add_error("number", "There is no such object in database!")
+        return cleaned_data
+
+
+class ChargeForm(forms.ModelForm):
+    class Meta:
+        model = Charge
+        fields = [
+            "value",
+            "date"
+        ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        value = cleaned_data.get('value')
+        charge_date = cleaned_data.get('date')
+
+        if value is None or date is None:
+            return cleaned_data
+        if value == Decimal(0):
+            self.add_error("value", "Charge can't be a zero value")
+        if value < 0 and charge_date > date.today():
+            self.add_error("date", "You can't set negative charge on future day")
+        return cleaned_data
---
+++ homework_7/finances/views.py
@@ -0,0 +1,164 @@
+from django.contrib import messages
+from django.db import transaction
+from django.db.models import Sum
+from django.db.models.functions import Extract
+from django.db.models.functions import TruncMonth
+from django.db.models.functions import TruncYear
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+from django.core.urlresolvers import reverse
+from django.db.models import Q
+
+from .calendar import get_month_name
+from .models import Account, Charge
+from .forms import ChargeForm, AccountForm, AccountLookForForm
+
+
+class MainPageView(generic.TemplateView):
+    template_name = 'main.html'
+    form_class = AccountForm
+    form_look_for_class = AccountLookForForm
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {
+            "title": "Main Page",
+            "form": self.form_class,
+            "form_look_for": self.form_look_for_class
+        })
+
+
+class AccountInsertView(generic.TemplateView):
+    template_name = 'main.html'
+    form_class = AccountForm
+    form_look_for_class = AccountLookForForm
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        form_look_for = self.form_look_for_class
+
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.save()
+            success_message = "Form successfully validated!"
+            info_message = "You created new Account(" \
+                           "number: " + str(request.POST.get('number')) \
+                           + ")"
+
+            messages.success(request, success_message)
+            messages.info(request, info_message)
+            return HttpResponseRedirect(instance.get_absolute_url())
+
+        return render(request, self.template_name, {
+            "title": "Finances",
+            "form": form,
+            "form_look_for": form_look_for
+        })
+
+
+class AccountSearchView(generic.TemplateView):
+    template_name = 'main.html'
+    form_class = AccountForm
+    form_look_for_class = AccountLookForForm
+
+    def post(self, request, *args, **kwargs):
+        form_look_for = self.form_look_for_class(request.POST)
+        form = self.form_class
+
+        if form_look_for.is_valid():
+            return HttpResponseRedirect(reverse('finances:account', args=[request.POST.get('number')]))
+
+        return render(request, self.template_name, {
+            "title": "Finances",
+            "form": form,
+            "form_look_for": form_look_for
+        })
+
+
+class AccountView(generic.FormView):
+    template_name = "account.html"
+
+    @transaction.atomic()
+    def get(self, request, number=None, *args, **kwargs):
+        account = get_object_or_404(Account, number=number)
+        deposit = Charge.objects.filter(account=account, value__gt=0.0).order_by('date')
+        withdraw = Charge.objects.filter(account=account, value__lt=0.0).order_by('date')
+        return render(request, self.template_name, {
+            "title": "Account page",
+            "deposit": deposit,
+            "withdraw": withdraw,
+            "account_number": account.number
+        })
+
+
+class AddChargeView(generic.FormView):
+    template_name = "add_charge.html"
+    form_class = ChargeForm
+    title_name = "Add new Charge"
+
+    def get(self, request, number=None, *args, **kwargs):
+        get_object_or_404(Account, number=number)
+        return render(request, self.template_name, {
+            "title": self.title_name,
+            "form": self.form_class,
+            "account_number": number
+        })
+
+    def post(self, request, number=None, *args, **kwargs):
+        get_object_or_404(Account, number=number)
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.account_id = number
+            instance.save()
+            success_message = "Form successfully validated!"
+            info_message = "You created new Charge(" \
+                           "value: " + str(request.POST.get('value')) + \
+                           ", date: " + request.POST.get('date') + ")"
+
+            messages.success(request, success_message)
+            messages.info(request, info_message)
+        return render(request, self.template_name, {
+            "title": self.title_name,
+            "form": form,
+            "account_number": number
+        })
+
+
+class AccountStatisticsView(generic.FormView):
+    template_name = "statistics.html"
+
+    def get_my_data(self, vars, acc=None):
+        if len(vars) == 0:
+            return acc
+        else:
+            a, b, c = vars.pop(-1)
+            if acc is None:
+                acc = {a: {get_month_name(b): c}}
+            else:
+                if acc.get(a) is not None:
+                    if get_month_name(b) not in acc[a]:
+                        acc[a][get_month_name(b)] = c
+                else:
+                    acc[a] = {get_month_name(b): c}
+            acc_new = self.get_my_data(vars,acc)
+            return acc_new
+
+    def get(self, request, number=None, *args, **kwargs):
+        account = get_object_or_404(Account, number=number)
+        stats2 = (Charge.objects
+                 .filter(account=account)
+                 .annotate(month=Extract('date', 'month'))
+                 .values('month')
+                 .annotate(total=Sum('value'))
+                 .annotate(year=Extract('date', 'year'))
+                 .values('year', 'month', 'total')
+                 .order_by('year', 'month')
+                 .values_list('year','month','total'))
+        stats = self.get_my_data(list(stats2))
+        return render(request, self.template_name, {
+            "title": "Account Statistics",
+            "data": stats,
+            "account_number": number
+        })
+
