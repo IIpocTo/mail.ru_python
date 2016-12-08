45609ac9ec3664a6399f5a36c88f0c11d1d45b75
George <eakarpov@yandex.ru>
2016-12-08 19:24:19 +0300

    Properer implementation of the decorator

--- additional_tasks/task_2_1/task2_1.py
+++ additional_tasks/task_2_1/task2_1.py
@@ -3,14 +3,11 @@ from time import time
 from random import randint
 
 
-def f(param, *args, **kwargs):
-    def decorator(*args, **kwargs):
+def constrained(param):
+    def decorator(f):
         def wrapper(*args, **kwargs):
             start = time()
-            delay = randint(1, 10)
-            sleep(delay)
             res = f(*args, **kwargs)
-            print("Function has been executed.")
             cont = time() - start
             if cont > param:
                 print("The program has been executing for " + str(cont) + " seconds,")
@@ -19,11 +16,14 @@ def f(param, *args, **kwargs):
     return decorator
 
 
-@f(8)
-def simple_sum(a, b):
-    return a + b
+@constrained(8)
+def f(message):
+    delay = randint(1, 10)
+    sleep(delay)
+    print(message)
+    return
 
 
 if __name__ == "__main__":
     for i in range(10):
-        simple_sum(i, i+1)
+        f("I have been executed! :)")
