f959aca3f7033e37943c82364480cee379910f49
George <eakarpov@yandex.ru>
2016-12-13 01:55:31 +0300

    User list first implementation

--- final_project/finances/admin/urls.py
+++ final_project/finances/admin/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 from .views import (
-    AdminMainView
+    AdminMainView, AdminUserListView
 )
 
 urlpatterns = [
     url(r'^$', AdminMainView.as_view(), name='main'),
+    url(r'^users/$', AdminUserListView.as_view(), name='user_list'),
 ]
--- final_project/finances/admin/views.py
+++ final_project/finances/admin/views.py
@@ -1,5 +1,6 @@
 from django.views import generic
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.exceptions import PermissionDenied
 
 from ..models import Account, Charge, UserProfile
@@ -13,5 +14,37 @@ class AdminMainView(generic.TemplateView):
             if request.user.is_staff:
                 return render(request, self.template_name, {
                     "title": "Main page"
+                })
+        raise PermissionDenied
+
+
+class AdminUserListView(generic.TemplateView):
+    template_name = "user_list.html"
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            if request.user.is_staff:
+                user_list = UserProfile.objects.filter(is_staff=False)
+                paginator = Paginator(user_list, 10)
+                page_next = None
+                page_previous = None
+                page = request.GET.get('page')
+                try:
+                    users = paginator.page(page)
+                    page_next = page - 1
+                    page_previous = page + 1
+                except PageNotAnInteger:
+                    users = paginator.page(1)
+                    page_next = 2
+                    page_next = None
+                except EmptyPage:
+                    users = paginator.page(paginator.num_pages)
+                    page_previous = paginator.num_pages - 1
+                    page_next = None
+
+                return render(request, self.template_name, {
+                    'users': users,
+                    'page_next': page_next,
+                    'page_previous': page_previous
                 })
         raise PermissionDenied
