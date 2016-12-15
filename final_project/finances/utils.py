189db6227ee41f8b8b0daf77e302eed63dda2a86
George <eakarpov@yandex.ru>
2016-12-15 14:40:25 +0300

    Fixed bugs on statistics page

--- final_project/finances/utils.py
+++ final_project/finances/utils.py
@@ -18,18 +18,43 @@ def get_month_name(month):
     return months[month]
 
 
+def get_elem(arr, el):
+    for elem in arr:
+        key, val = elem
+        if key == el:
+            return True
+    return False
+
+
+def get_index(arr, el):
+    i = 0
+    for elem in arr:
+        fst, snd = elem
+        if fst == el:
+            return i
+        i = i + 1
+    return None
+
+def get_annual(arr, el):
+    res = []
+    for elem in arr:
+        year, months = elem
+        if year == el:
+            res = months
+    return res
+
 def transform_data(variables, acc=None):
     if len(variables) == 0:
         return acc
     else:
         year, month, total = variables.pop(-1)
         if acc is None:
-            acc = {year: {get_month_name(month): total}}
+            acc = [[year, [[get_month_name(month), total]]]]
         else:
-            if acc.get(year) is not None:
-                if get_month_name(month) not in acc[year]:
-                    acc[year][get_month_name(month)] = total
+            if get_elem(acc, year):
+                months = get_annual(acc, year)
+                if not get_elem(months, get_month_name(month)):
+                    acc[get_index(acc, year)][1].append([get_month_name(month), total])
             else:
-                acc[year] = {get_month_name(month): total}
-        acc_new = transform_data(variables, acc)
-        return acc_new
+                acc.append([year, [[get_month_name(month), total]]])
+        return transform_data(variables, acc)
